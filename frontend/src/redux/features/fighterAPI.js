e305a5efe7077ed9d4b68381b3a788a435b3ce2a
huy-dataguy <quochuy.working@gmail.com>
2025-10-28 02:54:52 +0700

    update address api

--- frontend/src/redux/features/fighterAPI.js
+++ frontend/src/redux/features/fighterAPI.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import getBaseUrl from '../../utils/getBaseUrl'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `${getBaseUrl()}/api/fighter`,
+    baseUrl: `${getBaseUrl()}/api/fighters`,
     credentials: 'include',
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
