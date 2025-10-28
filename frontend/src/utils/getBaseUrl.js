f1cd682c58232a962d4f96441a12a27b86c3d9e5
huy-dataguy <quochuy.working@gmail.com>
2025-10-28 17:55:02 +0700

    vovinam hcmute with luv

--- frontend/src/utils/getBaseUrl.js
+++ frontend/src/utils/getBaseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
-    return 'http://localhost:3000';
-    // return 'https://vovinamspkt-be.vercel.app';
+    // return 'http://localhost:3000';
+    return 'https://vovinamspkt-be.vercel.app';
 
 }
 export default getBaseUrl;
