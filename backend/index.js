b484eb9bbce51def6dbc8690e1eff6a3795b1629
huy-dataguy <quochuy.working@gmail.com>
2025-10-28 17:58:08 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://vovinamspkt.vercel.app"],
+    origin: ["http://localhost:5173", "https://vovinamhcmute.vercel.app/"],
     credentials: true
 }))
 
