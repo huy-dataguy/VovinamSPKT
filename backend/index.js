b967b1c9929772fbdc70a6e017e38179a7de5fd8
huy-dataguy <quochuy.working@gmail.com>
2025-11-01 01:14:21 +0700

    add compress nodejs

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,48 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const compression = require('compression');
 
-const cors = require('cors')
-const app = express()
+const app = express();
 
+//Middleware
 
-//middleware
-app.use(express.json())
+//secure HTTP headers
+app.use(helmet());
+
+// log request cli
+app.use(morgan('dev'));
+
+app.use(compression());
+
+app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://vovinamhcmute.vercel.app"],
-    credentials: true
-}))
-
-
-//routes
+  origin: ["http://localhost:5173", "https://vovinamhcmute.vercel.app"],
+  credentials: true
+}));
 
 app.use("/api/fighters", require("./src/routes/fighter.route"));
-app.use('/api/tournaments', require("./src/routes/tournament.route"));
-app.use('/api/matches', require("./src/routes/match.route"));
-app.use('/api/auth', require("./src/routes/adminAuth"));
+app.use("/api/tournaments", require("./src/routes/tournament.route"));
+app.use("/api/matches", require("./src/routes/match.route"));
+app.use("/api/auth", require("./src/routes/adminAuth"));
 
-
-
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL);
-    // routes
-    app.use('/', (req, res) => {
-        res.send('this is my server')
-    })
+  await mongoose.connect(process.env.DB_URL);
+  console.log("Mongodb connected successfully!");
+
+  app.get('/', (req, res) => {
+    res.send('Server is running securely and fast!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 }
 
-main().then( () => console.log("Mongodb connect successfully!")).catch(err => console.log(err))
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+main().catch(err => console.error("MongoDB connection error:", err));
